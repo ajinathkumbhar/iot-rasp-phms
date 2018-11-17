88efc1fe0d46f86172e58c8bb87b5d60fc32fb8d
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-11-17 18:48:42 +0000

    Add Temperature and humidity module

--- app/phmsApp.py
+++ app/phmsApp.py
@@ -1,10 +1,16 @@
 import os
+from sensors.THSensor import THSensor
 
+THSens = THSensor() 
+
+def dumpBoardInfo():
+    print '----------------------'
+    print '1. ' + THSens.getSensorName()
+    print '----------------------'
 
 def main():
     print '---start----'
-
-
+    dumpBoardInfo()
 
 
 
---
+++ app/sensors/THSensor.py
@@ -0,0 +1,18 @@
+import os
+
+
+class THSensor:
+    def __init__(self):
+        self.name = "DHT11"
+
+    def getSensorName(self):
+        return self.name
+    
+    def getCurrTemp(self):
+        print 'impl  getCurrTemp()'
+        
+    def getCurrHumidity(self):
+        print 'Impl getCurrHumidity()'
+
+
+    
