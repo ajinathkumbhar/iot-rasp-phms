4a3fd36219b9fed31c82f75238a6a0ed7ec8da60
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-11-25 09:26:24 +0000

    create thread toread sensor data

--- app/phmsApp.py
+++ app/phmsApp.py
@@ -1,20 +1,52 @@
 import os
 from sensors.THSensor import THSensor
 from sensors.OledDisplay import OLEDDisplay 
+import threading
 
 THSens = THSensor()
 Disp = OLEDDisplay()
+
+humidity=
+temp=
+
 
 def dumpBoardInfo():
     print '----------------------'
     print '1. ' + THSens.getSensorName()
     print '----------------------'
 
+def getTHSensData():
+    humidity, temperature = THSens.getTHdata()
+    print('main Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+   
+def getGyroAccSensData():
+    print 'GryroAcc data ' + str(53)
+
+def getHBSensData():
+    print 'HBdata  ' + str(24)
+    
+def getSensorsData():
+    THThread = threading.Thread(target=getTHSensData, name='getTHSensData')
+    GyroAccThread = threading.Thread(target=getGyroAccSensData, name='getGyroAccSensData')
+    HBThread = threading.Thread(target=getHBSensData, name='getHBSensData')
+    THThread.start()
+    GyroAccThread.start()
+    HBThread.start()
+
+    GyroAccThread.join()
+    HBThread.join()
+    THThread.join()
+    
 def main():
     print '---start----'
     dumpBoardInfo()
     Disp.setupDisplay()
-    Disp.testDisplay();
+    #Disp.testDisplay()
+    getSensorsData()
+
+
+
+    
 
 if __name__ == '__main__':
     main()
--- app/sensors/THSensor.py
+++ app/sensors/THSensor.py
@@ -1,5 +1,8 @@
 import os
+import Adafruit_DHT
 
+sensor= Adafruit_DHT.DHT11
+pin = 4
 
 class THSensor:
     def __init__(self):
@@ -13,6 +16,11 @@ class THSensor:
         
     def getCurrHumidity(self):
         print 'Impl getCurrHumidity()'
-
-
     
+    def getTHdata(self):
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        if humidity is None or temperature is None:
+           print('Failed to get reading. Try again!')
+        return humidity, temperature
+        
+        
