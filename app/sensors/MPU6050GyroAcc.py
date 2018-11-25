4896b5ef6d5c2d0fc03a5bbfa8b77a4b2a4fd6ac
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-11-25 10:34:12 +0000

    MPU6050GyroAcc :class :

--- app/sensors/MPU6050GyroAcc.py
+++ app/sensors/MPU6050GyroAcc.py
@@ -18,8 +18,17 @@ GYRO_XOUT_H  = 0x43
 GYRO_YOUT_H  = 0x45
 GYRO_ZOUT_H  = 0x47
 
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
 
-def MPU_Init():
+class MPU6050GryroAcc:
+    def __init__(self):
+        self.name = "MPU6050 gryo acclerometer"
+
+    def setup():
+        MPU_Init()
+        
+    def MPU_Init():
 	#write to sample rate register
 	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
 	
@@ -35,7 +44,7 @@ def MPU_Init():
 	#Write to interrupt enable register
 	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
 
-def read_raw_data(addr):
+    def read_raw_data(addr):
 	#Accelero and Gyro value are 16-bit
         high = bus.read_byte_data(Device_Address, addr)
         low = bus.read_byte_data(Device_Address, addr+1)
@@ -47,18 +56,9 @@ def read_raw_data(addr):
         if(value > 32768):
                 value = value - 65536
         return value
-
-
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
-Device_Address = 0x68   # MPU6050 device address
-
-MPU_Init()
-
-print (" Reading Data of Gyroscope and Accelerometer")
-
-while True:
-	
-	#Read Accelerometer raw value
+    
+    def getData():
+        #Read Accelerometer raw value
 	acc_x = read_raw_data(ACCEL_XOUT_H)
 	acc_y = read_raw_data(ACCEL_YOUT_H)
 	acc_z = read_raw_data(ACCEL_ZOUT_H)
@@ -81,4 +81,4 @@ while True:
 	#print ("Gx=%.2f" %Gx, u'\u00b0'+ "/s", "\tGy=%.2f" %Gy, u'\u00b0'+ "/s", "\tGz=%.2f" %Gz, u'\u00b0'+ "/s", "\tAx=%.2f g" %Ax, "\tAy=%.2f g" %Ay, "\tAz=%.2f g" %Az) 	
         print ("Gx=%.2f" %Gx, "Gy=%.2f" %Gy, "Gz=%.2f" %Gz, "Ax=%.2f g" %Ax, "Ay=%.2f g" %Ay, "Az=%.2f g" %Az)
         #print 'Gyro : x={0:2f} y={1:2f} z={2:2f}  --- Acc : x={3:2f} y={4:2f} z={5:2f}'.format(Gx, Gy, Gz, Gx, Gy, Gz)
-	sleep(1)
+
