f9ee61d6a2a4267483bcabb7a0f3e45a4d421e91
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-12-17 15:27:12 +0000

    Setup : update boardconfig script for io.adafruit module

--- setup/BoardConfigSetup.sh
+++ setup/BoardConfigSetup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt-get update && 
 sudo apt-get install vim openssh-server openssh-client &&
+sudo apt-get install mosquitto
+sudo pip install paho-mqtt
+sudo pip install adafruit-io
 sudo systemctl enable ssh 
 sudo systemctl start ssh
 sudo apt-get install -y python-imaging python-smbus i2c-tools
