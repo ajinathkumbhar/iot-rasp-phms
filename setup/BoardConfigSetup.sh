7262abde706ab58c30b179e710d0f06b3f65d71a
ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-02-10 08:30:00 +0000

    setup : disable static ip setup

--- setup/BoardConfigSetup.sh
+++ setup/BoardConfigSetup.sh
@@ -7,9 +7,9 @@ sudo pip install adafruit-io
 sudo systemctl enable ssh 
 sudo systemctl start ssh
 sudo apt-get install -y python-imaging python-smbus i2c-tools
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
-chmod +x SetStaticIP.sh
-sudo ./SetStaticIP.sh
+#sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+#sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+#chmod +x SetStaticIP.sh
+#sudo ./SetStaticIP.sh
 
 
