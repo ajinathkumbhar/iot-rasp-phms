a7a30bb150727d8544bfba9bff089b4f92a7b040
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-02-24 13:24:22 +0530

    alert : add buzzer api

--- app/sensors/alert.py
+++ app/sensors/alert.py
@@ -2,16 +2,19 @@ import os
 from other import utils
 import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
 from time import sleep  # Import the sleep function from the time module
+from gpiozero import Buzzer
 
 # LED_PULSE=31
 # LED_TEMP=33
 # LED_ACC_EVENT=37
 # LED_ALERT=35
 
+# Fllowing are GPIO number (not a pin number)
 LED_PULSE=26
 LED_TEMP=19
 LED_ACC_EVENT=13
 LED_ALERT=6
+BUZZER_ALERT=21
 
 TAG = os.path.basename(__file__)
 class Alert:
@@ -21,6 +24,7 @@ class Alert:
         self.temp_limit = 40
         self.humidity_limit = 100
         self.ges_event_finger_limit = 3
+        self.buzzer = Buzzer(BUZZER_ALERT)
         GPIO.setwarnings(False)  # Ignore warning for now
         GPIO.setmode(GPIO.BCM)  # Use physical pin numbering
         GPIO.setup(LED_PULSE, GPIO.OUT, initial=GPIO.LOW)  # Set pin 8 to be an output pin and set initial value to low (off)
@@ -39,6 +43,7 @@ class Alert:
         if int(rate) < self.pulse_limit:
             return
         self.__trigger_led(LED_PULSE,0.3)
+        self.__trigger_buzzer()
         utils.PLOGD(TAG,"High pulse rate : " + str(rate))
 
     def __temp(self,val):
@@ -48,6 +53,7 @@ class Alert:
         if int(val) < self.temp_limit:
             return
         self.__trigger_led(LED_TEMP,0.3)
+        self.__trigger_buzzer()
         utils.PLOGD(TAG,"High body temperature: " + str(val))
 
     def __trigger_led(self,LED_PIN,blink_delay=0):
@@ -57,3 +63,8 @@ class Alert:
         GPIO.output(LED_PIN, GPIO.LOW)  # Turn off
         sleep(blink_delay)  # Sleep for 1 second
 
+    def __trigger_buzzer(self,timeout=0.5,beep=False):
+        self.buzzer.off()
+        self.buzzer.on()
+        sleep(timeout)
+        self.buzzer.off()
