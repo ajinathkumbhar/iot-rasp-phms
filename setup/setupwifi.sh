4e84768a3dd121ee8842d7f59ae702d41b90816c
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-03-02 22:13:16 +0530

    setupwifi : improve setup wifi

--- setup/setupwifi.sh
+++ setup/setupwifi.sh
@@ -1,5 +1,15 @@
-cat << EOF > wpa_supplicant.conf
+#!/usr/bin/env bash
 
+# check wifi connect with predefined
+# ssid
+IS_WIFI_CONNECTED=$(iwgetid |grep wlan0)
+if [[ "$IS_WIFI_CONNECTED" == *"TX2"* ]]; then
+    echo "wi-fi connected to TX2"
+    return
+fi
+
+# create wpa config file
+cat << EOF > wpa_supplicant.conf
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
 country=US
@@ -11,6 +21,7 @@ network={
 }
 EOF
 
+# copy wpa config file and restart wifi
 sudo cp wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 sudo wpa_cli -i wlan0 reconfigure
 rm wpa_supplicant.conf
