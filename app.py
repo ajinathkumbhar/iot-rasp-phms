cf762f24ae096c389c72fb3df0c597a34389911e
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-03-03 12:45:50 +0530

    gui : add first basic ui

---
+++ app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import Tkinter as tk
+import time
+import os
+import threading
+import signal
+import sys
+from phms import PhmsCore
+
+win = tk.Tk()
+win.title("Phms")
+#win.overrideredirect(True)
+win.geometry("480x320")
+lThreadsID = []
+
+stop_threads = False
+
+start_button = tk.Button(win, text='Start', bg='bisque2', height=1, width=26)
+start_button.grid(row=1, sticky=tk.NSEW)
+exit_button = tk.Button(win, text='Exit', bg='bisque2', height=1, width=6)
+exit_button.grid(row=2, sticky=tk.E)
+
+phms_core = PhmsCore()
+
+def signalHandler(sig,frame):
+    print 'You pressed ctrl+c'
+    print lThreadsID
+    if len(lThreadsID) == 0:
+        sys.exit(0)
+    for threadId in lThreadsID:
+        print 'killing thread ' + str(threadId)
+        threadId._Thread__stop()
+    phms_core.stop_app()
+    sys.exit(0)
+
+def run_app():
+    phms_core.start_app()
+
+def start_app():
+    app_thread = threading.Thread(target=run_app)
+    lThreadsID.append(app_thread)
+    app_thread.start()
+    start_button.configure(state="disabled")
+
+def exit_app():
+    os.kill(os.getpid(), signal.SIGTERM)
+
+def main():
+    signal.signal(signal.SIGTERM,signalHandler)
+    signal.signal(signal.SIGINT,signalHandler)
+
+    start_button.configure(command=start_app)
+    exit_button.configure(command=exit_app)
+
+
+if __name__ == "__main__":
+    main()
+
+tk.mainloop()
