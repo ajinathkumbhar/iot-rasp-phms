80cba7489dab7fa8bc1190eca28e10d311650ccc
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-03-06 23:08:33 +0530

    setup : update install script

--- setup/install3rdPartylib.sh
+++ setup/install3rdPartylib.sh
@@ -1,15 +1,31 @@
-cat << EOF >> /etc/dhcpcd.conf
+#!/bin/bash
 
-# Example static IP configuration:
-interface eth0
-static ip_address=192.168.0.11/24
-#static ip6_address=fd51:42f8:caae:d92e::ff/64
-static routers=192.168.0.1
-static domain_name_servers=192.168.0.1 8.8.8.8 fd51:42f8:caae:d92e::1
+echo "-----------------------------------------"
+echo " install Adafruit_Python_ADS1x15"
+echo "-----------------------------------------"
 
-interface wlan0
-static ip_address=192.168.0.10/24
-#static ip6_address=fd51:42f8:caae:d92e::ff/64
-static routers=192.168.0.1
-static domain_name_servers=192.168.0.1 8.8.8.8 fd51:42f8:caae:d92e::1
-EOF
+cd ../lib/Adafruit_Python_ADS1x15
+sudo python setup.py install
+cd ../
+
+echo "-----------------------------------------"
+echo " install mpu6050"
+echo "-----------------------------------------"
+cd ../lib/mpu6050
+sudo python setup.py install
+cd ../
+
+echo "-----------------------------------------"
+echo " install Adafruit_Python_SSD1306"
+echo "-----------------------------------------"
+cd ../lib/Adafruit_Python_SSD1306
+sudo python setup.py install
+cd ../
+
+echo "-----------------------------------------"
+echo " install Adafruit_Python_DHT"
+echo "-----------------------------------------"
+cd ../lib/Adafruit_Python_DHT
+sudo python setup.py install
+cd ../
+
