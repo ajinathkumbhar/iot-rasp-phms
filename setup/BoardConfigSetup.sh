5177175bf061ac386ba21028de5e7ca326adad1a
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2019-03-06 23:25:15 +0530

    setup: update install script for touchscreen setup_2

--- setup/BoardConfigSetup.sh
+++ setup/BoardConfigSetup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-git clone https://github.com/Elecrow-keen/Elecrow-LCD35.git
-cd Elecrow-LCD35
-sudo ./Elecrow-LCD35
-cd ../
-sudo apt-get update && 
+sudo apt-get update &&
 sudo apt-get install vim openssh-server openssh-client &&
 sudo apt-get install python-pip
 sudo apt-get install mosquitto
@@ -16,6 +12,11 @@ sudo systemctl enable ssh
 sudo systemctl start ssh
 sudo apt-get install -y python-imaging python-smbus i2c-tools
 bash install3rdPartylib.sh
+git clone https://github.com/Elecrow-keen/Elecrow-LCD35.git
+cd Elecrow-LCD35
+sudo ./Elecrow-LCD35
+cd ../
+
 #sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
 #sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 #chmod +x SetStaticIP.sh
